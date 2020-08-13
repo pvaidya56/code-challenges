1e3d2d46174faa86115ffa6d3d49cd5b68acf8c9
Priya <pvaidya56@gmail.com>
2020-08-13 13:19:24 -0700

    fix return value to return indicies

--- DayOne.js
+++ DayOne.js
@@ -16,7 +16,7 @@ var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             if (nums[j] == target - nums[i]) {
-                return ([nums[i], nums[j]])
+                return ([i, j])
             }
         }
     }
