c0cfff0b8341702bb7f42c22b47b23f9fd3f10b2
Priya <pvaidya56@gmail.com>
2020-09-23 13:54:26 -0700

    add more effiecient solution

--- Day4.js
+++ Day4.js
@@ -17,3 +17,31 @@ function sockMerchant(n, ar) {
      //return pairs
      return pairs;
  }
+
+ sockMerchant(9,
+    [10, 20, 20, 10, 10, 30, 50, 10, 20]);
+
+// ** For faster time complexity ** //
+
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+    //declare an object to store the array, for faster time 
+    let arObj = {};
+    //delare a variable to hold the count for pairs
+     let pairs = 0;
+     //iterate through the array
+     for(let i = 0; i < n; i++){
+         //check if index is already in obj, if so then it is a pair
+         if (arObj.hasOwnProperty(ar[i])){
+             pairs++
+             delete arObj[ar[i]];
+         }
+         else {
+             arObj[ar[i]] = 0;
+         }
+     }
+     //return pairs
+     return pairs;
+ }
+ sockMerchant(9,
+    [10, 20, 20, 10, 10, 30, 50, 10, 20]);
